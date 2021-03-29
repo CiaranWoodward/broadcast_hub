e11b544c011463007ca34a493c9daa1be8a08e73
Ciaran Woodward <ciaranwoodward@gmail.com>
2021-03-29 13:51:15 +0100

    Seperate server sending into seperate goroutine

--- server/server.go
+++ server/server.go
@@ -20,6 +20,8 @@ type serverClient struct {
 	cid msg.ClientId
 	// Relayed message stream (buffered)
 	relayMsgs chan msg.RelayIndication
+	// Response message stream (non-buffered)
+	responseMsgs chan msg.Message
 	// Message stream decoder
 	tc msg.Transcoder
 	dc msg.StreamDecoder
@@ -76,8 +78,34 @@ func (s *Server) startDispatcher(sc serverClient) {
 	}()
 }
 
+func (s *Server) startSender(sc serverClient) {
+	// Write messages to the transport, prioritising responses over relayed messages
+	go func() {
+		relay_mid := uint32(0)
+		for {
+			mesg := msg.Message{}
+			select {
+			case mesg = <-sc.responseMsgs:
+			default:
+				select {
+				case mesg = <-sc.responseMsgs:
+				case relayed := <-sc.relayMsgs:
+					mesg.Version = msg.MyVersion
+					mesg.MessageId = relay_mid
+					mesg.RelayInd = &relayed
+					relay_mid++
+				}
+			}
+			// Actually send the message
+			if sc.sendMessage(mesg) == msg.CONNECTION_ERROR {
+				break
+			}
+		}
+	}()
+}
+
 // Handle an incoming ID Request Message
-func (s *Server) handleIdRequest(sc *serverClient, mesg *msg.Message) msg.Status {
+func (s *Server) handleIdRequest(sc *serverClient, mesg *msg.Message) {
 	rsp := msg.Message{
 		Version:   msg.MyVersion,
 		MessageId: mesg.MessageId,
@@ -85,11 +113,11 @@ func (s *Server) handleIdRequest(sc *serverClient, mesg *msg.Message) msg.Status
 			Id: sc.cid,
 		},
 	}
-	return sc.sendMessage(rsp)
+	sc.responseMsgs <- rsp
 }
 
 // Handle an incoming List Request Message (This has a potentially unbounded response size. Limited by number of connected clients.)
-func (s *Server) handleListRequest(sc *serverClient, mesg *msg.Message) msg.Status {
+func (s *Server) handleListRequest(sc *serverClient, mesg *msg.Message) {
 	rsp := msg.Message{
 		Version:   msg.MyVersion,
 		MessageId: mesg.MessageId,
@@ -97,7 +125,7 @@ func (s *Server) handleListRequest(sc *serverClient, mesg *msg.Message) msg.Stat
 			Others: s.getClientIds(sc.cid),
 		},
 	}
-	return sc.sendMessage(rsp)
+	sc.responseMsgs <- rsp
 }
 
 // Handle an incoming Relay Request Message
@@ -155,16 +183,18 @@ func (s *Server) addClientByConnection(c net.Conn) {
 	// Generate CID, add it to the map, start the dispatcher for it
 	new_cid := msg.ClientId(atomic.AddUint64((*uint64)(&s.cid), 1))
 	new_sc := serverClient{
-		cid:       new_cid,
-		relayMsgs: make(chan msg.RelayIndication, maxBufferedMessages),
-		tc:        &msg.CborTranscoder{},
-		dc:        msg.NewCborStreamDecoder(c),
-		con:       c,
+		cid:          new_cid,
+		relayMsgs:    make(chan msg.RelayIndication, maxBufferedMessages),
+		responseMsgs: make(chan msg.Message),
+		tc:           &msg.CborTranscoder{},
+		dc:           msg.NewCborStreamDecoder(c),
+		con:          c,
 	}
 	s.clients_mutex.Lock()
 	s.clients[new_cid] = new_sc
 	s.clients_mutex.Unlock()
 	s.startDispatcher(new_sc)
+	s.startSender(new_sc)
 }
 
 // Remove a client from server mapping
@@ -187,6 +217,7 @@ func (s *Server) getClientIds(except_cid msg.ClientId) []msg.ClientId {
 	return cids
 }
 
+// Encode and send a message over the transport to the client
 func (sc *serverClient) sendMessage(m msg.Message) msg.Status {
 	encoded_msg, ok := sc.tc.Encode(m)
 	if !ok {
